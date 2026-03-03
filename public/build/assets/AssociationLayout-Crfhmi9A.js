722dd0e2d1772f58178facb6d6864b02f3b9c83e
Wai Mike <waimichael35@gmail.com>
2026-03-03 10:26:33 +0200

    Build assets

--- public/build/assets/AssociationLayout-Crfhmi9A.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./app-BVeP6S2y.js";import{G as s}from"./GuestLayout-C2eQggMc.js";function i({children:o}){return t.jsx(s,{children:o})}export{i as A};
--- public/build/assets/index-DzhGZCAg.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-BVeP6S2y.js";function a(){return r.jsx("div",{children:"User Dashboard"})}export{a as default};
