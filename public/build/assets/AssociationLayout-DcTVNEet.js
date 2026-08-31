ece65d196b8cd228f83c5354d187e56fb8f97f1c
Konson22 <konakech3@gmail.com>
2026-08-31 10:53:33 +0200

    added back up data

---
+++ public/build/assets/AssociationLayout-DcTVNEet.js
@@ -0,0 +1,1 @@
+import{j as t}from"./app-DX2rYbfP.js";import{G as s}from"./GuestLayout-DKpEc7Mp.js";function i({children:o}){return t.jsx(s,{children:o})}export{i as A};
---
+++ public/build/assets/index-Dku_2Mx2.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-DX2rYbfP.js";function a(){return r.jsx("div",{children:"User Dashboard"})}export{a as default};
